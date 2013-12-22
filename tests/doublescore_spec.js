0e82d8c8065f157e1234398e36f3c2a6c96557e9
Eno Compton 4 <eno4@ecom.com>
2013-12-22 13:02:23 -0800

    Add basic __.each function

--- tests/doublescore_spec.js
+++ tests/doublescore_spec.js
@@ -14,4 +14,30 @@ describe("doublescore.js", function() {
       expect(__.keys(empty)).toEqual([]);
     });
   });
+
+  describe("__.each", function() {
+    it("iterates over an array with a function for each element", function() {
+      var obj = [1, 2, 3],
+          memo = [],
+          iterator = function(item) {
+            memo.push(item);
+          };
+
+      __.each(obj, iterator);
+
+      expect(memo).toEqual([1, 2, 3]);
+    });
+
+    it("takes an iterator whose arguments include an optional index", function() {
+      var obj = [1, 2, 3],
+          memo = {},
+          iterator = function(item, index) {
+            memo[item] = index;
+          };
+
+      __.each(obj, iterator);
+
+      expect(memo).toEqual({1: 0, 2: 1, 3: 2});
+    });
+  });
 });
