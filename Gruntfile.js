040407f880e04bf2e1abd7e41a0fcca8ee68690c
Eno Compton 4 <eno4@ecom.com>
2013-12-22 10:55:49 -0800

    Add jasmine to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,24 @@ module.exports = function(grunt) {
 
   // Project configuration
   grunt.initConfig({
+    jasmine: {
+      src: "lib/**/*.js",
+      options: {
+        specs: "tests/**/*.js"
+      },
+    },
     jshint: {
       options: {
         jshintrc: ".jshintrc"
       },
-      all: ["Gruntfile.js"]
+      all: ["Gruntfile.js", "tests/**/*.js", "lib/**/*.js"]
     }
   });
 
   // Load plugins
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-jasmine");
 
   // Default task
-  grunt.registerTask("default", ["jshint"]);
+  grunt.registerTask("default", ["jshint", "jasmine"]);
 };
