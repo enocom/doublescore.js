79bb543ceec8855877b9dc3276622d8a08202669
Eno Compton 4 <eno4@ecom.com>
2013-12-23 00:38:30 -0700

    Remove top level nesting in test

--- tests/doublescore_spec.js
+++ tests/doublescore_spec.js
@@ -1,92 +1,90 @@
-describe("doublescore.js", function() {
-  describe("__.keys", function() {
-    it("returns an array of an object's keys", function() {
-      var obj = {
-        "foo": true,
-        "bar": false
-      };
+describe("__.keys", function() {
+  it("returns an array of an object's keys", function() {
+    var obj = {
+      "foo": true,
+  "bar": false
+    };
 
-      expect(__.keys(obj)).toEqual(["foo", "bar"]);
-    });
-
-    it("returns an empty array if the object is empty", function() {
-      var empty = {};
-      expect(__.keys(empty)).toEqual([]);
-    });
+    expect(__.keys(obj)).toEqual(["foo", "bar"]);
   });
 
-  describe("__.each", function() {
-    it("iterates over an array calling a function for each item", function() {
-      var arr = [1, 2, 3],
-          memo = [],
-          iterator = function(item) {
-            memo.push(item);
-          };
+  it("returns an empty array if the object is empty", function() {
+    var empty = {};
+    expect(__.keys(empty)).toEqual([]);
+  });
+});
 
-      __.each(arr, iterator);
+describe("__.each", function() {
+  it("iterates over an array calling a function for each item", function() {
+    var arr = [1, 2, 3],
+    memo = [],
+    iterator = function(item) {
+      memo.push(item);
+    };
 
-      expect(memo).toEqual([1, 2, 3]);
-    });
+  __.each(arr, iterator);
 
-    it("takes an iterator with an optional index", function() {
-      var arr = [1, 2, 3],
-          memo = {},
-          iterator = function(item, index) {
-            memo[item] = index;
-          };
+  expect(memo).toEqual([1, 2, 3]);
+  });
 
-      __.each(arr, iterator);
+  it("takes an iterator with an optional index", function() {
+    var arr = [1, 2, 3],
+    memo = {},
+    iterator = function(item, index) {
+      memo[item] = index;
+    };
 
-      expect(memo).toEqual({1: 0, 2: 1, 3: 2});
-    });
+  __.each(arr, iterator);
 
-    it("may reference the original array in the iterator", function() {
-      var arr = [1],
-          iterator = function(item, index, obj) {
-            obj.push("foo");
-          };
+  expect(memo).toEqual({1: 0, 2: 1, 3: 2});
+  });
 
-      __.each(arr, iterator);
+  it("may reference the original array in the iterator", function() {
+    var arr = [1],
+    iterator = function(item, index, obj) {
+      obj.push("foo");
+    };
 
-      expect(arr).toEqual([1, "foo"]);
-    });
+  __.each(arr, iterator);
 
-    it("takes an optional context", function() {
-      var arr = ["foo"],
-          thisArg = {
-            thisArgProperty: false
-          },
-          iterator = function() {
-            this.thisArgProperty = true;
-          };
+  expect(arr).toEqual([1, "foo"]);
+  });
 
-      __.each(arr, iterator, thisArg);
+  it("takes an optional context", function() {
+    var arr = ["foo"],
+    thisArg = {
+      thisArgProperty: false
+    },
+    iterator = function() {
+      this.thisArgProperty = true;
+    };
 
-      expect(thisArg.thisArgProperty).toBeTruthy();
-    });
+  __.each(arr, iterator, thisArg);
 
-    it("also takes an object", function() {
-      var obj = {one: 1, two: 2},
-          memo = [],
-          iterator = function(value, key, obj) {
-            memo.push(value, key, obj);
-          };
+  expect(thisArg.thisArgProperty).toBeTruthy();
+  });
 
-      __.each(obj, iterator);
+  it("also takes an object", function() {
+    var obj = {one: 1, two: 2},
+    memo = [],
+    iterator = function(value, key, obj) {
+      memo.push(value, key, obj);
+    };
 
-      var expected = [1, "one", {one: 1, two: 2}, 2, "two", {one: 1, two: 2}];
-      expect(memo).toEqual(expected);
-    });
+  __.each(obj, iterator);
 
-    it("does nothing when passed null", function() {
-      var memo = [],
-          iterator = function(item) {
-            memo.push(item);
-          };
+  var expected = [1, "one", {one: 1, two: 2}, 2, "two", {one: 1, two: 2}];
+  expect(memo).toEqual(expected);
+  });
 
-      __.each(null, iterator);
+  it("does nothing when passed null", function() {
+    var memo = [],
+    iterator = function(item) {
+      memo.push(item);
+    };
 
-      expect(memo).toEqual([]);
-    });
+  __.each(null, iterator);
+
+  expect(memo).toEqual([]);
   });
 });
