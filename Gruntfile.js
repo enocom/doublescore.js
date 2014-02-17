1cb1d9549fd7d5dd67e89da3a1eca239dc563f82
Eno Compton 4 <eno4@ecom.com>
2014-02-17 13:33:48 -0800

    Add uglify plugin
    Because ugly and compact JavaScript is awesome.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,29 @@ module.exports = function(grunt) {
   // Project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+
     jasmine: {
       src: "lib/**/*.js",
       options: {
         specs: "tests/**/*.js"
       },
     },
+
     jshint: {
       options: {
         jshintrc: ".jshintrc"
       },
       all: ["Gruntfile.js", "tests/**/*.js", "lib/**/*.js"]
     },
+
+    uglify: {
+      my_target: {
+        files: {
+          'output.min.js': ['lib/doublescore.js']
+        }
+      }
+    },
+
     watch: {
       scripts: {
         files: ["Gruntfile.js", "lib/**/*.js", "tests/**/*.js"],
@@ -26,6 +37,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-jasmine");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
   // Default task
   grunt.registerTask("default", ["jshint", "jasmine"]);
